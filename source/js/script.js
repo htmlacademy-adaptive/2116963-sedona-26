9b835b864e524b8d54d088667d37dfcb1af6a431
alex-v-10 <alex.ru9311@gmail.com>
2022-08-17 00:14:11 +0300

    refactor: change less nesting

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,7 @@
+/**
+ * * Navigation menu open/close
+ */
+
 let nav = document.querySelector('.navigation');
 let navToggle = document.querySelector('.navigation__toggle');
 
@@ -6,3 +10,9 @@ nav.classList.remove('navigation--active');
 navToggle.addEventListener('click', function() {
   nav.classList.toggle('navigation--active');
 });
+
+/**
+ * * Form validation with modal windows
+ */
+
+//TODO
