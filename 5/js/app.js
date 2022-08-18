6b10de6cde29dba6d2b1a01571a7f35e769de755
Keks <keks@htmlacademy.ru>
2022-08-19 01:18:44 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/app.js
@@ -0,0 +1,41 @@
+/**
+ * * Navigation menu open/close
+ */
+
+let nav = document.querySelector('.navigation');
+let navToggle = document.querySelector('.navigation__toggle');
+
+nav.classList.remove('navigation--nojs');
+
+navToggle.addEventListener('click', function () {
+  nav.classList.toggle('navigation--active');
+});
+
+/**
+ * * Form modal windows
+ */
+
+let form = document.querySelector('.review__form');
+let formSubmit = document.querySelector('.form__submit');
+let modalError = document.querySelector('.modal--error');
+let modalSuccess = document.querySelector('.modal--success');
+let modalButtonError = document.querySelector('.modal__button--error');
+let modalButtonSuccess = document.querySelector('.modal__button--success');
+let active = 'modal--active';
+
+form.addEventListener('submit', function () {
+  modalError.classList.remove(active);
+  modalSuccess.classList.add(active);
+});
+
+formSubmit.addEventListener('click', function () {
+  modalError.classList.add(active);
+});
+
+modalButtonError.addEventListener('click', function () {
+  modalError.classList.remove(active);
+});
+
+modalButtonSuccess.addEventListener('click', function () {
+  modalSuccess.classList.remove(active);
+});
