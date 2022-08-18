897294189f52b17f69fbe2a5fdc15f7620d9804e
alex-v-10 <alex.ru9311@gmail.com>
2022-08-18 16:14:28 +0300

    feat: add js to modal windows

--- source/js/script.js
+++ source/js/script.js
@@ -7,7 +7,7 @@ let navToggle = document.querySelector('.navigation__toggle');
 
 nav.classList.remove('navigation--active');
 
-navToggle.addEventListener('click', function() {
+navToggle.addEventListener('click', function () {
   nav.classList.toggle('navigation--active');
 });
 
@@ -15,4 +15,37 @@ navToggle.addEventListener('click', function() {
  * * Form validation with modal windows
  */
 
-//TODO
+let form = document.querySelector('.review__form');
+let formSubmit = document.querySelector('.form__submit');
+let modalError = document.querySelector('.modal--error');
+let modalSuccess = document.querySelector('.modal--success');
+let modalButtons = document.querySelectorAll('.modal__button');
+let active = 'modal--active';
+
+form.addEventListener('submit', function () {
+  modalSuccess.classList.add(active);
+});
+
+formSubmit.addEventListener('click', function () {
+  let fields = form.querySelectorAll('input[required]');
+  let email = form.querySelector('input[name="email"]');
+  let emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  for (let i = 0; i < fields.length; i++) {
+    if (fields[i].value === '') {
+      modalError.classList.add(active);
+      return;
+    }
+  }
+
+  if (!emailRegex.test(email.value.toLowerCase())) {
+    modalError.classList.add(active);
+  }
+});
+
+modalButtons.forEach(function (modalButton) {
+  modalButton.addEventListener('click', function () {
+    modalError.classList.remove(active);
+    modalSuccess.classList.remove(active);
+  });
+});
