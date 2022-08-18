a6df04e17a093685a21dc0be000b24ae6b27f801
alex-v-10 <alex.ru9311@gmail.com>
2022-08-18 17:08:45 +0300

    fix: change modal js

--- source/js/script.js
+++ source/js/script.js
@@ -19,33 +19,23 @@ let form = document.querySelector('.review__form');
 let formSubmit = document.querySelector('.form__submit');
 let modalError = document.querySelector('.modal--error');
 let modalSuccess = document.querySelector('.modal--success');
-let modalButtons = document.querySelectorAll('.modal__button');
+let modalButtonError = document.querySelector('.modal__button--error');
+let modalButtonSuccess = document.querySelector('.modal__button--success');
 let active = 'modal--active';
 
 form.addEventListener('submit', function () {
+  modalError.classList.remove(active);
   modalSuccess.classList.add(active);
 });
 
 formSubmit.addEventListener('click', function () {
-  let fields = form.querySelectorAll('input[required]');
-  let email = form.querySelector('input[name="email"]');
-  let emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-  for (let i = 0; i < fields.length; i++) {
-    if (fields[i].value === '') {
-      modalError.classList.add(active);
-      return;
-    }
-  }
-
-  if (!emailRegex.test(email.value.toLowerCase())) {
-    modalError.classList.add(active);
-  }
+  modalError.classList.add(active);
 });
 
-modalButtons.forEach(function (modalButton) {
-  modalButton.addEventListener('click', function () {
-    modalError.classList.remove(active);
-    modalSuccess.classList.remove(active);
-  });
+modalButtonError.addEventListener('click', function () {
+  modalError.classList.remove(active);
+});
+
+modalButtonSuccess.addEventListener('click', function () {
+  modalSuccess.classList.remove(active);
 });
